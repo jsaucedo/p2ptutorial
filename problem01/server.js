ebdf565290b303be31d4c8c1902fe2c035e9c07b
Juan Ignacio Saucedo <jsaucedo.dev@gmail.com>
2016-11-26 18:58:42 -0300

    correct naming on problem01 server

--- problem01/server.js
+++ problem01/server.js
@@ -1,17 +1,17 @@
 var net = require('net')
 
-var server = net.createServer((server) => {
-  server.on('data', (input) => {
-    console.log('server.remotePort is: ', server.remotePort)
-    server.write(input)
+var server = net.createServer((socket) => {
+  socket.on('data', (input) => {
+    console.log('socket.remotePort is: ', socket.remotePort)
+    socket.write(input)
   })
 
-  console.log('server.localPort is: ', server.localPort)
-  console.log('server.remotePort is: ', server.remotePort)
+  console.log('socket.localPort is: ', socket.localPort)
+  console.log('socket.remotePort is: ', socket.remotePort)
 })
 
 server.on('connection', () => {
-  console.log('new connection!');
+  console.log('new connection!')
 })
 
 server.listen(8888, () => {
